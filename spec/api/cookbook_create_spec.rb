de23c05eaf97e6ec3f1431184c100196dfd66cba
Tristan O'Neil <tristanoneil@gmail.com>
2014-04-09 10:48:09 -0400

    Abstract much of share_cookbook into SignedRequestHelper class for reuse between other authenticated API endpoints.

--- spec/api/cookbook_create_spec.rb
+++ spec/api/cookbook_create_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'POST /api/v1/cookbooks' do
   context 'the user provides valid params' do
-    before(:each) { share_cookbook('redis-test') }
+    before(:each) { share_cookbook(cookbook_name: 'redis-test') }
 
     it 'returns a 201' do
       expect(response.status.to_i).to eql(201)
@@ -14,7 +14,7 @@ describe 'POST /api/v1/cookbooks' do
   end
 
   context "the user doesn't provide valid params" do
-    before(:each) { share_cookbook('redis-test', payload: {}) }
+    before(:each) { share_cookbook(cookbook_name: 'redis-test', payload: {}) }
 
     it 'returns a 400' do
       expect(response.status.to_i).to eql(400)
@@ -30,7 +30,7 @@ describe 'POST /api/v1/cookbooks' do
   end
 
   context "the user sharing doesn't exist" do
-    before(:each) { share_cookbook('redis-test', user: double(:user, username: 'non-existent-user')) }
+    before(:each) { share_cookbook(cookbook_name: 'redis-test', user: double(:user, username: 'non-existent-user')) }
 
     it 'returns a 401' do
       expect(response.status.to_i).to eql(401)
@@ -46,7 +46,7 @@ describe 'POST /api/v1/cookbooks' do
   end
 
   context "the users private/public key pair is invalid" do
-    before(:each) { share_cookbook('redis-test', private_key: 'invalid_private_key.pem') }
+    before(:each) { share_cookbook(cookbook_name: 'redis-test', private_key: 'invalid_private_key.pem') }
 
     it 'returns a 401' do
       expect(response.status.to_i).to eql(401)
@@ -62,10 +62,18 @@ describe 'POST /api/v1/cookbooks' do
   end
 
   context "invalid signing headers are sent" do
-    before(:each) { share_cookbook('redis-test', omitted_headers: ['HTTP_X_OPS_SIGN']) }
+    before(:each) { share_cookbook(cookbook_name: 'redis-test', omitted_headers: ['X-Ops-Sign']) }
 
     it 'returns a 400' do
       expect(response.status.to_i).to eql(400)
+    end
+
+    it 'returns an error code' do
+      expect(json_body['error_code']).to_not be_nil
+    end
+
+    it 'returns an error message' do
+      expect(json_body['error_messages']).to_not be_nil
     end
   end
 end
--- spec/support/api_spec_helpers.rb
+++ spec/support/api_spec_helpers.rb
@@ -4,60 +4,84 @@ require 'tempfile'
 require 'mixlib/authentication/signedheaderauth'
 
 module ApiSpecHelpers
-  def share_cookbook(cookbook_name, options = {})
-    if options.has_key?(:user)
-      user = options[:user]
-    else
-      user = create(:user)
-      user.accounts << create(:account, provider: 'chef_oauth2')
+  class SignedRequestHelper
+    include FactoryGirl::Syntax::Methods
+
+    def initialize(options = {})
+      @cookbook_name = options.fetch(:cookbook_name, 'supermarket')
+      @private_key_name = options.fetch(:private_key, 'valid_private_key.pem')
+      @custom_metadata = options.fetch(:custom_metadata, {})
+      @omitted_headers = options.fetch(:omitted_headers, [])
+      @request_path = options.fetch(:request_path, '/api/v1/cookbooks')
+      @request_method = options.fetch(:request_method, 'post')
+
+      if options.has_key?(:user)
+        @user = options[:user]
+      else
+        @user = create(:user)
+        @user.accounts << create(:account, provider: 'chef_oauth2')
+      end
     end
 
+    def tarball
+      metadata = {
+        name: @cookbook_name,
+        version: '1.0.0',
+        description: "Installs/Configures #{@cookbook_name}",
+        maintainer: 'Chef Software, Inc',
+        license: 'MIT',
+        platforms: {
+          'ubuntu' => '>= 12.0.0'
+        },
+        dependencies: {
+          'apt' => '~> 1.0.0'
+        }
+      }.merge(@custom_metadata)
+
+      tarball = Tempfile.new([@cookbook_name, '.tgz'], 'tmp').tap do |file|
+        io = AndFeathers.build(@cookbook_name) do |base_dir|
+          base_dir.file('README.md') { '# README' }
+          base_dir.file('metadata.json') do
+            JSON.dump(metadata)
+          end
+        end.to_io(AndFeathers::GzippedTarball)
+
+        file.write(io.read)
+        file.rewind
+      end
+    end
+
+    def signed_headers
+      signed_headers = Mixlib::Authentication::SignedHeaderAuth.signing_object({
+        http_method: @request_method,
+        path: @request_path,
+        user_id: @user.username,
+        timestamp: Time.now.utc.iso8601,
+        body: tarball.read
+      }).sign(private_key)
+
+      @omitted_headers.each { |h| signed_headers.delete(h) }
+
+      signed_headers
+    end
+
+    private
+
+    def private_key
+      OpenSSL::PKey::RSA.new(
+        File.read("spec/support/key_fixtures/#{@private_key_name}")
+      )
+    end
+  end
+
+  def share_cookbook(options = {})
+    sr = SignedRequestHelper.new(options)
     category = create(:category, name: options.fetch(:category, 'other').titleize)
 
-    private_key = OpenSSL::PKey::RSA.new(
-      File.read("spec/support/key_fixtures/#{options.fetch(:private_key, 'valid_private_key.pem')}")
-    )
-
-    metadata = {
-      name: cookbook_name,
-      version: '1.0.0',
-      description: "Installs/Configures #{cookbook_name}",
-      maintainer: 'Chef Software, Inc',
-      license: 'MIT',
-      platforms: {
-        'ubuntu' => '>= 12.0.0'
-      },
-      dependencies: {
-        'apt' => '~> 1.0.0'
-      }
-    }.merge(options.fetch(:custom_metadata, {}))
-
-    tarball = Tempfile.new([cookbook_name, '.tgz'], 'tmp').tap do |file|
-      io = AndFeathers.build(cookbook_name) do |base_dir|
-        base_dir.file('README.md') { '# README' }
-        base_dir.file('metadata.json') do
-          JSON.dump(metadata)
-        end
-      end.to_io(AndFeathers::GzippedTarball)
-
-      file.write(io.read)
-      file.rewind
-    end
-
-    tarball_upload = fixture_file_upload(tarball.path, 'application/x-gzip')
+    tarball_upload = fixture_file_upload(sr.tarball.path, 'application/x-gzip')
     payload = options.fetch(:payload, { cookbook: "{\"category\": \"#{category.name}\"}", tarball: tarball_upload })
 
-    signed_header = Mixlib::Authentication::SignedHeaderAuth.signing_object({
-      http_method: 'post',
-      path: '/api/v1/cookbooks',
-      user_id: user.username,
-      timestamp: Time.now.utc.iso8601,
-      body: tarball.read
-    }).sign(private_key)
-
-    options.fetch(:omitted_headers, []).each { |h| signed_header.delete(h) }
-
-    post '/api/v1/cookbooks', payload, signed_header
+    post '/api/v1/cookbooks', payload, sr.signed_headers
   end
 
   def json_body
